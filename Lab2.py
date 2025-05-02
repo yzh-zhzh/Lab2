86231ff056608b2603c4799694c9c67cd96129d4
Yao Zhenghan <zhenghan.24@ichat.sp.edu.sg>
2025-05-02 17:14:34 +0800

    Updated Lab2 File

--- Lab2.py
+++ Lab2.py
@@ -3,32 +3,31 @@ def display_main_menu():
 
 def get_user_input():
     temp = input("Enter some temperature values, separated by spaces: ")
-    temp = temp.split() #split string into list
-    temp = [float(i) for i in temp] #convert string to float
+    temp = temp.split()
+    temp = [float(i) for i in temp]
     return temp
 
 def calc_average(temp_list):
-    if len(temp_list) == 0: #check if the list is empty
+    if len(temp_list) == 0:
         return 0
     return sum(temp_list) / len(temp_list)
 
 def find_min_max(temp_list):
-    if len(temp_list) == 0: #check if the list is empty
+    if len(temp_list) == 0:
         return 0, 0 
     return min(temp_list), max(temp_list)
 
 def sort_temperature(temp_list):
-    temp_list.sort() #sort function to sort the list
+    temp_list.sort()
     return temp_list
 
 def calc_median_temperature(temp_list):
     temp_list.sort()
     if len(temp_list) % 2 == 0:
-        return (temp_list[len(temp_list)//2] + temp_list[len(temp_list)//2-1]) / 2 #check if the list is even
+        return (temp_list[len(temp_list)//2] + temp_list[len(temp_list)//2-1]) / 2
     else:
-        return temp_list[len(temp_list)//2] #check if the list is odd
+        return temp_list[len(temp_list)//2]
 
-#must call the function to run the code
 temp_list = get_user_input()
 average = calc_average(temp_list)
 min_temp, max_temp = find_min_max(temp_list)
